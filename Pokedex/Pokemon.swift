e8fd03a503721de060e6995dd3f4fd2e18c3a423
Vinz <nguyenhvubk@gmail.com>
2016-12-28 21:23:26 +0700

    finish design and created pokemon class

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,32 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by HOANGVU on 12/28/16.
+//  Copyright © 2016 HOANGVU. All rights reserved.
+//
+
+import Foundation
+
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int){
+        
+        self._name = name
+        self._pokedexId = pokedexId
+        
+    }
+    
+}
