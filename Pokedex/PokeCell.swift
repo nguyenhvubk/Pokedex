e84ece0b2bcea41b06d36431a98a5d345ddae2b4
Vinz <nguyenhvubk@gmail.com>
2016-12-28 22:45:41 +0700

    Implement CollectionView

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,29 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by HOANGVU on 12/28/16.
+//  Copyright © 2016 HOANGVU. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon){
+        self.pokemon = pokemon
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
